05715afd6b14cfa59095611c64ad509efe0f5fe1
evilieswithin <akshayurankar48@gmail.com>
2022-05-18 09:22:32 +0530

    last two changes

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,21 @@ import Results from "../components/Results";
 import requests from "../utils/requests";
 
 export default function Home({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
-        <title>Hulu 2.0</title>
+        <title>Hulu Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* Header */}
       <Header />
 
+      {/* Nav */}
       <Nav />
 
+      {/* Results */}
       <Results results={results} />
     </div>
   );
@@ -26,7 +30,7 @@ export async function getServerSideProps(context) {
 
   const request = await fetch(
     `https://api.themoviedb.org/3${
-      requests[genre]?.url || request.fetchTrending.url
+      requests[genre]?.url || requests.fetchTrending.url
     }`
   )
     .then((res) => res.json())
